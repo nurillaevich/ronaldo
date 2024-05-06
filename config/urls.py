bc7a071e05be87265c032ae762eae124b5f49b3b
Firdavs Qudratillayev <147323638+nurillaevich@users.noreply.github.com>
2024-05-06 20:04:50 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from ronaldo.config import settings
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
